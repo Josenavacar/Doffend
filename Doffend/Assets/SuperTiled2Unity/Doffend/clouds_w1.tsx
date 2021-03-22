a2410a5c10c36ae3f8c3e94669e04157456bc9f3
Kenneth Luper <Kenneth.Luper@gmail.com>
2021-03-22 13:08:24 +0100

    Added level. Need to reimport bigger level at some point

---
+++ Doffend/Assets/SuperTiled2Unity/Doffend/clouds_w1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="clouds_w1" tilewidth="16" tileheight="16" tilecount="240" columns="20">
+ <image source="BG2.png" width="320" height="192"/>
+</tileset>
