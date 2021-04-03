534a13e9d8782de7389bc56c484e52ee1cec6791
Jose <>
2021-04-03 15:48:31 +0200

    Goblins spawn

--- Doffend/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Doffend/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -18,6 +18,12 @@ namespace Pathfinding {
 		public Transform target;
 		IAstarAI ai;
 
+		void Start()
+    	{
+       	  	GameObject Player = GameObject.Find("Player");
+			target = Player.transform;
+    	}
+
 		void OnEnable () {
 			ai = GetComponent<IAstarAI>();
 			// Update the destination right before searching for a path as well.
--- Doffend/Assets/EnemySpawn.cs
+++ Doffend/Assets/EnemySpawn.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject[] enemies;
-    public float spawnTime = 6f;
-    private Vector2 spawnPosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-       // InvokeRepeating("Spawn", spawnTime, spawnPosition);
-    }
+   public GameObject[] enemies;
+   public float spawnTime = 6f;
+   private Vector2 spawnPosition;
+   // Start is called before the first frame update
+   void Start()
+   {
+      InvokeRepeating("Spawn", spawnTime, spawnTime);
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   // Update is called once per frame
+   void Update()
+   {
+      
+   }
 
-    void Spawn()
-    {
-       // spawnPosition.x = 
-    }
+   void Spawn()
+   {
+   spawnPosition.x = 0.141f;
+   spawnPosition.y = -8.176f;
+
+   Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length -1)], spawnPosition, Quaternion.identity);
+   }
 }
