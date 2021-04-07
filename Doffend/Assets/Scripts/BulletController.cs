e73512344779c9b62cc8f513e392681a4f07f0cf
Jose <>
2021-04-07 11:25:56 +0200

    Wave Manager
    Changed approach to spawning waves, it's still W.I.P but we have 1 wave

--- Doffend/Assets/Scripts/BulletController.cs
+++ Doffend/Assets/Scripts/BulletController.cs
@@ -8,11 +8,6 @@ public class BulletController : MonoBehaviour
     [SerializeField]
     private float speed = 4f;
 
-    void Start()
-    {
-        
-    }
-
     IEnumerator DestroyBulletAfterTime()
     {
         yield return new WaitForSeconds(3f);
---
+++ Doffend/Assets/Scripts/WaveSpawn.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawn : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public GameObject enemy;
+        public int count;
+        public float rate;
+    }
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+    public Transform spawn;
+
+    private float searchCountdown = 1f;
+    private SpawnState state = SpawnState.COUNTING;
+
+    public Wave[] waves;
+    private int nextWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == SpawnState.WAITING)
+        {
+            if(!EnemyIsAlive())
+            {
+                state = SpawnState.COUNTING;
+                nextWave++;
+                //Finish current wave
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        } 
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if(searchCountdown <= 0)
+        {
+            searchCountdown = 1f;
+            if(GameObject.FindGameObjectWithTag("Goblin") == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+        for(int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f/_wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(GameObject _enemy)
+    {
+        Instantiate(_enemy, spawn.position, Quaternion.identity);
+        Debug.Log("Spawning enemy: " + _enemy.name);
+    }
+}
